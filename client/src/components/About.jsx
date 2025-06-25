b9d072de5edb1259f8d34ecac117cded2e2ac16c
Ayush Bunkar <ayushbunkar100@gmail.com>
2025-06-25 20:44:27 +0530

    about us code

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-       <section   className="h-200 w-full  bg-center relative  "
+       <section   className="min-h-screen w-full bg-no-repeat bg-center bg-cover relative flex items-center justify-center text-white"
       style={{
         backgroundImage:
-          "url('https://images.pexels.com/photos/169211/pexels-photo-169211.jpeg')",
+          "url('https://imgs.search.brave.com/jg0IqkgWmVWKvTJd65SPKEt3rLVT1rCtRY0apNQbsBI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA5LzkxLzE0LzA4/LzM2MF9GXzk5MTE0/MDg3N193a0VXSlEx/akx4NlBydTVFUUhi/SE9kRVpXSHhNTDI2/dy5qcGc')",
       }}> 
        <div className="absolute  inset-0 bg-[rgba(0,0,0,0.6)]  "></div>
        <div className="z-10 mt-30 text-center px-4 absolute top-10">
