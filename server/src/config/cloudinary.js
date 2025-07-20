11a3e62a8d271e10d4999cafc9cadc673d976b64
Ayush Bunkar <ayushbunkar100@gmail.com>
2025-07-20 18:13:58 +0530

    cvbn

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.dx7ztr9i3,
-  api_key: process.env.313846931262354,
+  api_key: 313846931262354,
   api_secret: process.env.f1TAhJDmQ9FWAu9HTnWacMy9Rn0,
 });
 
