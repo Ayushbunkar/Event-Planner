5865af5197aa0a45a09aa2ab900988b55c45c101
Ayush Bunkar <ayushbunkar100@gmail.com>
2025-08-17 23:56:09 +0530

    n

--- server/src/controllers/adminController.js
+++ server/src/controllers/adminController.js
@@ -10,7 +10,7 @@ export const GetAllContacts = async (req, res, next) => {
     next(error);
   }
 };
-// hjvnbxcvcxvnbmnnm gghj
+// hjvnbxcvcxvnbmnnm gghjsdvsd
 export const UpdateContacts = async (req, res, next) => {
   try {
     const QueryId = req.params.Qid;
