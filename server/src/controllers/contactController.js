51dce22111926047554d55567cba29fa7edade56
Ayush Bunkar <ayushbunkar100@gmail.com>
2025-08-28 22:15:21 +0530

    dg

--- server/src/controllers/contactController.js
+++ server/src/controllers/contactController.js
@@ -10,7 +10,7 @@ export const SubmitContactForm = async (req, res, next) => {
       return next(error);
     }
 
-    // dgsdsgdsfdfs
+    // dgsdsgdsfdfsvbxcb
 
     const newContact = await Contact.create({ fullname, email, feedback });
 
